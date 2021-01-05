daf809341d912dbd1912aedbe1c8fe6add629839
AARÓN RODRÍGUEZ-BARBERO GONZÁLEZ <Aharon1377@gmail.com>
2021-01-05 15:06:49 +0100

    database connection view finished (functionality not complete)

--- TFG/Startup.cs
+++ TFG/Startup.cs
@@ -50,7 +50,7 @@ namespace TFG
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=DatabaseConnection}/{id?}");
             });
         }
     }
